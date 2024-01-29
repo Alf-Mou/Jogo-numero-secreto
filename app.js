081169deb909999fc0ff8d699353b71bf417cf88
Alfredo Moura <alfred.mou99@gmail.com>
2024-01-28 22:33:03 -0300

    Refinando e finalizando projeto

--- app.js
+++ app.js
@@ -2,20 +2,18 @@ let numerosSorteados = [];
 let numeroSecreto = numeroAleatorio();
 let tentativas = 1;
 
-function exibirTexto(tag, texto) {
-    let campo = document.querySelector(tag)
-    campo.innerHTML = texto
-}
-
-function exibirMensagemInicial(){
-    exibirTexto(`h1`, `Jogo do número secreto`)
-    exibirTexto(`p`, `Chute um número entre 1 e 10` )
-}
+iniciarJogo()
 
 console.log(numeroSecreto)
-exibirMensagemInicial()
+
 function verificarChute() {
     let chute = document.querySelector(`input`).value
+
+    if (chute.trim() === ``){
+        alert(`Por favor, insira um número antes de chutar.`)
+        return;
+    }
+
     if (chute == numeroSecreto){
         exibirTexto(`h1`, `Parabéns, você conseguiu`)
         let palavraTentativa = tentativas > 1 ? `tentativas` : `tentativa`;
@@ -24,11 +22,13 @@ function verificarChute() {
 
     } else if (chute > numeroSecreto) {
         exibirTexto(`h1`, `Tente um número menor`)
-    } else {
+    } else{
         exibirTexto(`h1`, `Tente um número maior`)
-    }
+        }
     limparCampo()
-    tentativas++
+    if (chute.trim() !== '') {
+        tentativas++;
+    }
 }
 
 function limparCampo(){
@@ -39,7 +39,7 @@ function limparCampo(){
 function numeroAleatorio(){
     let numeroEscolhido = parseInt(Math.random() * 10 + 1);
     if(numerosSorteados.includes(numeroEscolhido)) {
-        return numeroAleatorio
+        return numeroAleatorio();
     } else {
         numerosSorteados.push(numeroEscolhido);
         console.log(numerosSorteados)
@@ -47,10 +47,25 @@ function numeroAleatorio(){
     }
 }
 
+function exibirTexto(tag, texto) {
+    let campo = document.querySelector(tag)
+    campo.innerHTML = texto
+}
+
+function mensagemInicial(){
+    exibirTexto(`h1`, `Jogo do número secreto`);
+    exibirTexto(`p`, `Chute um número entre 1 e 10`);
+}
+
+function iniciarJogo() {
+    mensagemInicial();
+}
+
 function reiniciarJogo() {
     numeroSecreto = numeroAleatorio();
-    exibirMensagemInicial()
-    limparCampo
-    tentativas = 1
+    limparCampo();
+    exibirTexto(`h1`, `Jogo do número secreto`);
+    exibirTexto(`p`, `Chute um número entre 1 e 10`);
+    tentativas = 1;
     document.getElementById('reiniciar').setAttribute('disabled', true);
 }
